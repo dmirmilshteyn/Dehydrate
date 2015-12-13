afea079b6cf0b1bc0016d43b08a9dd2176b7015e
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-12-13 08:22:15 -0500

    Added initial support for dehydrating assemblies

---
+++ src/Dehydrate/Dehydrator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Dehydrate
+{
+    public class Dehydrator
+    {
+        public void Dehydrate(IReadOnlyList<string> targetAssemblies, string outputDirectory) {
+            if (!Directory.Exists(outputDirectory)) {
+                Directory.CreateDirectory(outputDirectory);
+            }
+
+            foreach (var assembly in targetAssemblies) {
+                ProcessAssembly(assembly, Path.Combine(outputDirectory, Path.GetFileName(assembly)));
+            }
+        }
+
+        private void ProcessAssembly(string assemblyPath, string outputPath) {
+            using (ModuleDefMD module = ModuleDefMD.Load(assemblyPath)) {
+                foreach (var type in module.Types) {
+
+                    // Rules (for each type in the module):
+                    // * Remove all private fields
+                    // * Remove all private methods
+                    // * Clear the bodies for all public members
+                    // * Remove all private property getters
+                    // * Remove all private property setters
+                    // * Remove all properties without either a getter or setter 
+
+                    // Process fields
+                    for (int i = type.Fields.Count - 1; i >= 0; i--) {
+                        if (!type.Fields[i].IsPublic) {
+                            type.Fields.RemoveAt(i);
+                        }
+                    }
+
+                    // Process methods
+                    for (int i = type.Methods.Count - 1; i >= 0; i--) {
+                        if (type.Methods[i].IsPublic) {
+                            ClearMethodBody(type.Methods[i]);
+                        } else {
+                            type.Methods.RemoveAt(i);
+                        }
+                    }
+
+                    // Process properties
+                    for (int i = type.Properties.Count - 1; i >= 0; i--) {
+                        bool remove = true;
+
+                        // Go through all getter methods
+                        for (int g = type.Properties[i].GetMethods.Count - 1; g >= 0; g--) {
+                            if (type.Properties[i].GetMethods[g].IsPublic) {
+                                remove = false;
+                                ClearMethodBody(type.Properties[i].GetMethods[g]);
+                            } else {
+                                type.Properties[i].GetMethods.RemoveAt(g);
+                            }
+                        }
+
+                        // Go through all setter methods
+                        for (int s = type.Properties[i].SetMethods.Count - 1; s >= 0; s--) {
+                            if (type.Properties[i].SetMethods[s].IsPublic) {
+                                remove = false;
+                                ClearMethodBody(type.Properties[i].SetMethods[s]);
+                            } else {
+                                type.Properties[i].SetMethods.RemoveAt(s);
+                            }
+                        }
+
+                        // Remove this property if no methods were found
+                        if (remove) {
+                            type.Properties.RemoveAt(i);
+                        }
+                    }
+                }
+
+                module.Write(outputPath);
+            }
+        }
+
+        private void ClearMethodBody(MethodDef method) {
+            method.Body.Instructions.Clear();
+            method.Body.Instructions.Add(new Instruction(OpCodes.Ret));
+        }
+    }
+}
--- src/Dehydrate/Program.cs
+++ src/Dehydrate/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Framework.Runtime.Common.CommandLine;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,50 @@ namespace Dehydrate
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static int Main(string[] args) {
+            var app = new CommandLineApplication
+            {
+                Name = "Dehydrate",
+                Description = "Performs dehydration on a target assembly"
+            };
 
+            app.Command("dehydrate", c =>
+            {
+                c.Description = "Performs dehydration on the selected assembly.";
+
+                var reverseOption = c.Option("-r|--reverse", "Display the message in reverse", CommandOptionType.NoValue);
+                var messageArg = c.Argument("[message]", "The message you wish to display");
+                c.HelpOption("-?|-h|--help");
+
+                c.OnExecute(() =>
+                {
+                    var message = messageArg.Value;
+                    if (reverseOption.HasValue()) {
+                        message = new string(message.ToCharArray().Reverse().ToArray());
+                    }
+                    Console.WriteLine(message);
+                    return 0;
+                });
+            });
+
+            var targetAssemblies = app.Option("-a|--assemblies", "The target assembly to dehydrate", CommandOptionType.MultipleValue);
+            var outputDirectory = app.Option("-o|--output", "Output directory for dehydrated assemblies", CommandOptionType.SingleValue);
+            app.HelpOption("-?|-h|--help");
+
+            app.OnExecute(() =>
+            {
+                if (!outputDirectory.HasValue()) {
+                    Console.WriteLine("No output directory specified.");
+                    return 1;
+                }
+
+                var dehydrator = new Dehydrator();
+                dehydrator.Dehydrate(targetAssemblies.Values, Path.GetFullPath(outputDirectory.Value()));
+
+                return 2;
+            });
+
+            return app.Execute(args);
         }
     }
 }
